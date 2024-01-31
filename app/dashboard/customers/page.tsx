b2b08348b8334d42c794da8cb5799c65a03dba72
Minh Nguyen <minhktmd@gmail.com>
2024-01-31 23:27:43 +0700

    .

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,7 @@
+type Props = {};
+
+const Page = (props: Props) => {
+  return <div>customers</div>;
+};
+
+export default Page;
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,7 @@
+type Props = {};
+
+const Page = (props: Props) => {
+  return <div>Invoice</div>;
+};
+
+export default Page;
